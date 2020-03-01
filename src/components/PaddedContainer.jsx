7d50f9c189cd421f2f85798ed5c10bb0c23b1d1b
Roman Oskolkov <wild_neo@mail.ru>
2020-03-01 17:39:56 +0500

    add padded container

---
+++ src/components/PaddedContainer.jsx
@@ -0,0 +1,10 @@
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+const styles = {
+  root: {
+    padding: 16,
+  },
+};
+
+export default withStyles(styles)(Paper);
