15e5af25c26aa0092fa361034916991ab40add99
Roman Oskolkov <wild_neo@mail.ru>
2020-03-03 15:28:04 +0500

    add property selector

--- src/store/selectors/index.js
+++ src/store/selectors/index.js
@@ -1,3 +1,6 @@
+import { createSelector } from 'reselect';
+import { orderBy } from 'lodash';
+
 export const getConsumers = (type) => ({ consumersState: { consumptions } }) => (
   type
     ? consumptions.filter((item) => item.type === type)
@@ -7,3 +10,17 @@ export const getTableRowPerPage = (state) => state.tablesState.rowsPerPage;
 export const getActideDashboardTab = (state) => state.dashboardState.activeTab;
 export const getFilterConsumerType = (state) => state.filterState.consumerType;
 export const getFilterDateInterval = (state) => state.filterState.dateInterval;
+
+export const propertySelector = (type) => (prop, fn) => createSelector(
+  getConsumers(type),
+  (consumers) => {
+    const store = new Map();
+    consumers.forEach(({ [prop]: p }) => {
+      const item = fn ? fn(p): p;
+      store.set(item, item);
+    });
+    const values = Array.from(store.values());
+
+    return orderBy(values);
+  },
+);
